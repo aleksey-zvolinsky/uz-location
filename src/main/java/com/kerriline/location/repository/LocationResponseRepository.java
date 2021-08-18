1a67a71a05f666bc9c2c911a3da86c6d7a51e4d7
Aleksey Zvolinsky <aleksey@zvolinsky.name>
2021-08-18 22:21:21 +0300

    Update LocationResponseRepository.java

--- src/main/java/com/kerriline/location/repository/LocationResponseRepository.java
+++ src/main/java/com/kerriline/location/repository/LocationResponseRepository.java
@@ -1,7 +1,14 @@
 package com.kerriline.location.repository;
 
 import com.kerriline.location.domain.LocationResponse;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -9,4 +16,8 @@ import org.springframework.stereotype.Repository;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface LocationResponseRepository extends JpaRepository<LocationResponse, Long>, JpaSpecificationExecutor<LocationResponse> {}
+public interface LocationResponseRepository extends JpaRepository<LocationResponse, Long>, JpaSpecificationExecutor<LocationResponse> {
+    @Query("select r from LocationResponse r where r.responseDatetime > :responseDatetime order by responseDatetime desc")
+    List<LocationResponse> findAllWithResposeDateTimeAfter(
+      @Param("responseDatetime") LocalDate cutDate);
+}
