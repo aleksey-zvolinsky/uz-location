516b48e1a99f25cbddcc6e16c4e11d2596041e17
Oleksii Zvolinskyi <oleksii_zvolinskyi@epam.com>
2023-08-12 17:52:28 +0200

    Replaced scheduled manager with scheduled+retryable spring annotations

--- src/main/java/com/kerriline/location/ReportManager.java
+++ src/main/java/com/kerriline/location/ReportManager.java
@@ -1,5 +1,9 @@
 package com.kerriline.location;
 
+import com.kerriline.location.domain.LocationResponse;
+import com.kerriline.location.domain.Tank;
+import com.kerriline.location.repository.KerrilineLocationResponseRepository;
+import com.kerriline.location.repository.TankRepository;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -7,7 +11,6 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.security.GeneralSecurityException;
 import java.time.ZonedDateTime;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -17,9 +20,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-
-import javax.mail.MessagingException;
-
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -27,15 +27,7 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import com.kerriline.location.domain.LocationResponse;
-import com.kerriline.location.domain.Tank;
-import com.kerriline.location.mail.MailManager;
-import com.kerriline.location.mail.MailParser;
-import com.kerriline.location.repository.KerrilineLocationResponseRepository;
-import com.kerriline.location.repository.TankRepository;
 
 /**
  *
@@ -45,191 +37,148 @@ import com.kerriline.location.repository.TankRepository;
 @Component
 public class ReportManager {
 
-	private static final Logger LOG = LoggerFactory.getLogger(ReportManager.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ReportManager.class);
 
-	@Autowired
-    MailManager mail;
-	@Autowired
-    MailParser source;
-
-
-	@Value("${application.result-mail.to}")
-	private String mailTo = "service@kerriline.com.ua";
-
-	@Autowired
+    @Autowired
     TankRepository tankRepository;
 
-	@Autowired
-	KerrilineLocationResponseRepository locationResponseRepository;
-	
-	public File generateReport() throws GeneralSecurityException, IOException, MessagingException {
+    @Autowired
+    KerrilineLocationResponseRepository locationResponseRepository;
 
-		
-	    InputStream templateStream = Thread.currentThread().getContextClassLoader()
-	    		.getResourceAsStream("template.xlsx");
-		
-		String reportFile = Files.createTempFile("uz-location", ".xlsx").toString();
-		
-		Files.copy(templateStream, Paths.get(reportFile), StandardCopyOption.REPLACE_EXISTING);
-		
-		try(Workbook wb = WorkbookFactory.create(new File(reportFile))) {
-			
-			
-			Sheet tanksSheet = wb.getSheet("Вагоны");
-			
-			
-			List<Tank> tanks = tankRepository.findAll();
-			
-			int rownum = 2;
-			for (Tank tank : tanks) {
-				Row row = tanksSheet.createRow(rownum++);
-				int cellnum = 0;
-				row.createCell(cellnum++).setCellValue(rownum-2);
-				row.createCell(cellnum++).setCellValue(tank.getClientName());
-				row.createCell(cellnum++).setCellValue(tank.getOwnerName());
-				row.createCell(cellnum++).setCellValue(tank.getTankNumber());
-			}
-			
-			Map<String, Tank> tanksMap = tanks.stream()
-				.filter(distinctByKey(t -> t.getTankNumber()))
-				.collect(Collectors.toMap(Tank::getTankNumber, tank -> tank));
-			
-			ZonedDateTime offset = ZonedDateTime.now().minusDays(7);
+    public File generateReport() throws IOException {
+        InputStream templateStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("template.xlsx");
 
-			printToExcel(wb, "Результат", 
-					locationResponseRepository.findByResponseDatetimeAfter(offset), 
-					tanksMap, true);
-			
-			List<String> tankTypes = Arrays.asList("60 ПОЛУВАГОНЫ", "68 ГЛУХОДОННЫЕ");
-			
-			printToExcel(wb, "Полувагоны", 
-					locationResponseRepository.findByTankTypeInAndResponseDatetimeAfter(tankTypes, offset), 
-					tanksMap, false);
-			
-			printToExcel(wb, "Не полувагоны", 
-					locationResponseRepository.findByTankTypeNotInAndResponseDatetimeAfter(tankTypes, offset), 
-					tanksMap, false);
-			
-			String finalReportFile = Files.createTempFile("uz-location", ".xlsx").toString();
-			
-	        LOG.info("Storing file to {}", finalReportFile);
-	        
-	        wb.setForceFormulaRecalculation(true);
-	        
-	        try(FileOutputStream out = new FileOutputStream(finalReportFile)) {
-	        	wb.write(out);
-	        }
-	        
-	        return new File(finalReportFile);
-		}
-		
-	}
-	
+        String reportFile = Files.createTempFile("uz-location", ".xlsx").toString();
+
+        assert templateStream != null;
+        Files.copy(templateStream, Paths.get(reportFile), StandardCopyOption.REPLACE_EXISTING);
+
+        try (Workbook wb = WorkbookFactory.create(new File(reportFile))) {
+            Sheet tanksSheet = wb.getSheet("Вагоны");
+
+            List<Tank> tanks = tankRepository.findAll();
+
+            int rownum = 2;
+            for (Tank tank : tanks) {
+                Row row = tanksSheet.createRow(rownum++);
+                int cellnum = 0;
+                row.createCell(cellnum++).setCellValue(rownum - 2);
+                row.createCell(cellnum++).setCellValue(tank.getClientName());
+                row.createCell(cellnum++).setCellValue(tank.getOwnerName());
+                row.createCell(cellnum).setCellValue(tank.getTankNumber());
+            }
+
+            Map<String, Tank> tanksMap = tanks
+                .stream()
+                .filter(distinctByKey(Tank::getTankNumber))
+                .collect(Collectors.toMap(Tank::getTankNumber, tank -> tank));
+
+            ZonedDateTime offset = ZonedDateTime.now().minusDays(7);
+
+            printToExcel(wb, "Результат", locationResponseRepository.findByResponseDatetimeAfter(offset), tanksMap, true);
+
+            List<String> tankTypes = Arrays.asList("60 ПОЛУВАГОНЫ", "68 ГЛУХОДОННЫЕ");
+
+            printToExcel(
+                wb,
+                "Полувагоны",
+                locationResponseRepository.findByTankTypeInAndResponseDatetimeAfter(tankTypes, offset),
+                tanksMap,
+                false
+            );
+
+            printToExcel(
+                wb,
+                "Не полувагоны",
+                locationResponseRepository.findByTankTypeNotInAndResponseDatetimeAfter(tankTypes, offset),
+                tanksMap,
+                false
+            );
+
+            String finalReportFile = Files.createTempFile("uz-location", ".xlsx").toString();
+
+            LOG.info("Storing file to {}", finalReportFile);
+
+            wb.setForceFormulaRecalculation(true);
+
+            try (FileOutputStream out = new FileOutputStream(finalReportFile)) {
+                wb.write(out);
+            }
+
+            return new File(finalReportFile);
+        }
+    }
+
     public static <T> Predicate<T> distinctByKey(Function<? super T, Object> keyExtractor) {
         Map<Object, Boolean> map = new ConcurrentHashMap<>();
         return t -> map.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
     }
 
+    private void printToExcel(
+        Workbook wb,
+        String sheetName,
+        List<LocationResponse> responses,
+        Map<String, Tank> originalTanks,
+        boolean addEmptyTanks
+    ) {
+        Map<String, Tank> tanks = new HashMap<>(originalTanks);
 
-	private void printToExcel(Workbook wb, String sheetName, 
-			List<LocationResponse> responses, Map<String, Tank> originalTanks,
-			boolean addEmptyTanks) {
-		
-		Map<String, Tank> tanks = new HashMap<>();
-		tanks.putAll(originalTanks);
-		
-		
-		Sheet sh = wb.getSheet(sheetName);
-		
-		int rownum = 3;
-		for (LocationResponse response : responses) {
-				if(!tanks.containsKey(response.getTankNumber())) {
-					continue;
-				}	
-				
-				Tank tank = tanks.get(response.getTankNumber());
-				
-				Row row = sh.createRow(rownum++);
-				int cellnum = 0;
-				row.createCell(cellnum++)
-					.setCellValue(rownum-3);
-				row.createCell(cellnum++)
-					.setCellValue(tank.getClientName());
-				row.createCell(cellnum++)
-					.setCellValue(tank.getOwnerName());
-				row.createCell(cellnum++)
-					.setCellValue(response.getTankNumber());
-				row.createCell(cellnum++)
-					.setCellValue(response.getTankType());
-				row.createCell(cellnum++)
-					.setCellValue(response.getCargoId());
-				row.createCell(cellnum++)
-					.setCellValue(response.getCargoName());
-				row.createCell(cellnum++)
-					.setCellValue(response.getWeight());
-				row.createCell(cellnum++)
-					.setCellValue(response.getReceiverId());
-				row.createCell(cellnum++)
-					.setCellValue(response.getTankIndex());
-		        row.createCell(cellnum++)
-		        	.setCellValue(response.getLocationStationId());
-		        row.createCell(cellnum++)
-		            .setCellValue(response.getLocationStationName());
-		        row.createCell(cellnum++)
-		            .setCellValue(response.getLocationDatetime());
-		        row.createCell(cellnum++)
-		            .setCellValue(response.getLocationOperation());
-		        row.createCell(cellnum++)
-		            .setCellValue(response.getStateToStationId());
-		        row.createCell(cellnum++)
-		            .setCellValue(response.getStateToStationName());
-		        row.createCell(cellnum++)
-		            .setCellValue(response.getStateFromStationId());
-		        row.createCell(cellnum++)
-		            .setCellValue(response.getStateFromStationName());
-		        row.createCell(cellnum++)
-		            .setCellValue(response.getStateSendDatetime());
-		        row.createCell(cellnum++)
-		            .setCellValue(response.getStateSenderId());
-		        row.createCell(cellnum++)
-		            .setCellValue(response.getPlanedServiceDatetime());
-		        row.createCell(cellnum++)
-		            .setCellValue(response.getTankOwner());
-		        row.createCell(cellnum++)
-		            .setCellValue(response.getTankModel());
-		        row.createCell(cellnum++)
-		            .setCellValue(response.getDefectRegion());
-		        row.createCell(cellnum++)
-		            .setCellValue(response.getDefectStation());
-		        row.createCell(cellnum++)
-		            .setCellValue(response.getDefectDatetime());
-		        row.createCell(cellnum++)
-		            .setCellValue(response.getDefectDetails());
-		        row.createCell(cellnum++)
-		            .setCellValue(response.getRepairRegion());
-		        row.createCell(cellnum++)
-		            .setCellValue(response.getRepairStation());
-		        row.createCell(cellnum++)
-		            .setCellValue(response.getRepairDatetime());
-		        row.createCell(cellnum++)
-		            .setCellValue(response.getUpdateDatetime());
-		        // taking just first occurrence
-		        tanks.remove(response.getTankNumber());
-		}
-		
-		if (addEmptyTanks) {
-			for (Tank tank: tanks.values()) {
-				Row row = sh.createRow(rownum++);
-				int cellnum = 0;
-				row.createCell(cellnum++)
-					.setCellValue(rownum-3);
-				row.createCell(cellnum++)
-					.setCellValue(tank.getClientName());
-				row.createCell(cellnum++)
-					.setCellValue(tank.getOwnerName());
-				row.createCell(cellnum++)
-					.setCellValue(tank.getTankNumber());
-			}
-		}
-	}
+        Sheet sh = wb.getSheet(sheetName);
+
+        int rownum = 3;
+        for (LocationResponse response : responses) {
+            if (!tanks.containsKey(response.getTankNumber())) {
+                continue;
+            }
+
+            Tank tank = tanks.get(response.getTankNumber());
+
+            Row row = sh.createRow(rownum++);
+            int cellnum = 0;
+            row.createCell(cellnum++).setCellValue(rownum - 3);
+            row.createCell(cellnum++).setCellValue(tank.getClientName());
+            row.createCell(cellnum++).setCellValue(tank.getOwnerName());
+            row.createCell(cellnum++).setCellValue(response.getTankNumber());
+            row.createCell(cellnum++).setCellValue(response.getTankType());
+            row.createCell(cellnum++).setCellValue(response.getCargoId());
+            row.createCell(cellnum++).setCellValue(response.getCargoName());
+            row.createCell(cellnum++).setCellValue(response.getWeight());
+            row.createCell(cellnum++).setCellValue(response.getReceiverId());
+            row.createCell(cellnum++).setCellValue(response.getTankIndex());
+            row.createCell(cellnum++).setCellValue(response.getLocationStationId());
+            row.createCell(cellnum++).setCellValue(response.getLocationStationName());
+            row.createCell(cellnum++).setCellValue(response.getLocationDatetime());
+            row.createCell(cellnum++).setCellValue(response.getLocationOperation());
+            row.createCell(cellnum++).setCellValue(response.getStateToStationId());
+            row.createCell(cellnum++).setCellValue(response.getStateToStationName());
+            row.createCell(cellnum++).setCellValue(response.getStateFromStationId());
+            row.createCell(cellnum++).setCellValue(response.getStateFromStationName());
+            row.createCell(cellnum++).setCellValue(response.getStateSendDatetime());
+            row.createCell(cellnum++).setCellValue(response.getStateSenderId());
+            row.createCell(cellnum++).setCellValue(response.getPlanedServiceDatetime());
+            row.createCell(cellnum++).setCellValue(response.getTankOwner());
+            row.createCell(cellnum++).setCellValue(response.getTankModel());
+            row.createCell(cellnum++).setCellValue(response.getDefectRegion());
+            row.createCell(cellnum++).setCellValue(response.getDefectStation());
+            row.createCell(cellnum++).setCellValue(response.getDefectDatetime());
+            row.createCell(cellnum++).setCellValue(response.getDefectDetails());
+            row.createCell(cellnum++).setCellValue(response.getRepairRegion());
+            row.createCell(cellnum++).setCellValue(response.getRepairStation());
+            row.createCell(cellnum++).setCellValue(response.getRepairDatetime());
+            row.createCell(cellnum).setCellValue(response.getUpdateDatetime());
+            // taking just first occurrence
+            tanks.remove(response.getTankNumber());
+        }
+
+        if (addEmptyTanks) {
+            for (Tank tank : tanks.values()) {
+                Row row = sh.createRow(rownum++);
+                int cellnum = 0;
+                row.createCell(cellnum++).setCellValue(rownum - 3);
+                row.createCell(cellnum++).setCellValue(tank.getClientName());
+                row.createCell(cellnum++).setCellValue(tank.getOwnerName());
+                row.createCell(cellnum).setCellValue(tank.getTankNumber());
+            }
+        }
+    }
 }
--- src/main/java/com/kerriline/location/config/ApplicationProperties.java
+++ src/main/java/com/kerriline/location/config/ApplicationProperties.java
@@ -1,13 +1,8 @@
 package com.kerriline.location.config;
-import org.quartz.SchedulerException;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-
-import com.kerriline.location.LocationManager;
-import com.kerriline.location.SchedulerManager;
-import com.kerriline.location.mail.MailManager;
-import com.kerriline.location.mail.MailParser;
 
 /**
  * Properties specific to UZ Location.
@@ -15,142 +10,11 @@ import com.kerriline.location.mail.MailParser;
  * Properties are configured in the {@code application.yml} file.
  * See {@link tech.jhipster.config.JHipsterProperties} for a good example.
  */
-@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+@ConfigurationProperties(prefix = "application")
 public class ApplicationProperties {
 
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
-    }
-
-    @Bean
-    public MailManager mailManager() {
-        return new MailManager();
-    }
-
-    @Bean
-    public MailParser mailParser() {
-        return new MailParser();
-    }
-
-    private String locationSchedule = "0 45 7,13 ? * *I";
-
-    private String mileageSchedule = "0 45 5 ? * *I";
-
-    @Bean
-    public SchedulerManager schedulerManager() throws SchedulerException {
-        SchedulerManager manager = new SchedulerManager(locationSchedule, mileageSchedule);
-        return manager;
-    }
-
-    @Bean
-    public LocationManager locationManager() throws SchedulerException {
-        LocationManager manager = new LocationManager();
-        return manager;
-    }
-
-    public String getLocationSchedule() {
-        return locationSchedule;
-    }
-
-    public String getMileageSchedule() {
-        return mileageSchedule;
-    }
-
-    public void setLocationSchedule(String locationSchedule) {
-        this.locationSchedule = locationSchedule;
-    }
-
-    public void setMileageSchedule(String mileageSchedule) {
-        this.mileageSchedule = mileageSchedule;
-    }
-
-    private ReportMail reportMail = new ReportMail();
-
-    private ResultMail resultMail = new ResultMail();
-
-    public ReportMail getReportMail() {
-        return reportMail;
-    }
-
-    public void setReportMail(ReportMail reportMail) {
-        this.reportMail = reportMail;
-    }
-
-    public ResultMail getResultMail() {
-        return resultMail;
-    }
-
-    public void setResultMail(ResultMail resultMail) {
-        this.resultMail = resultMail;
-    }
-
-    public static class ReportMail {
-        private String login;
-        private String password;
-        private String to;
-
-        public String getLogin() {
-            return login;
-        }
-
-        public void setLogin(String login) {
-            this.login = login;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
-        public String getTo() {
-            return to;
-        }
-
-        public void setTo(String to) {
-            this.to = to;
-        }
-    }
-
-    public static class ResultMail {
-        private String attachmentFilename;
-        private String subject;
-        private String text;
-        private String to;
-
-        public String getAttachmentFilename() {
-            return attachmentFilename;
-        }
-
-        public void setAttachmentFilename(String attachmentFilename) {
-            this.attachmentFilename = attachmentFilename;
-        }
-
-        public String getSubject() {
-            return subject;
-        }
-
-        public void setSubject(String subject) {
-            this.subject = subject;
-        }
-
-        public String getText() {
-            return text;
-        }
-
-        public void setText(String text) {
-            this.text = text;
-        }
-
-        public String getTo() {
-            return to;
-        }
-
-        public void setTo(String to) {
-            this.to = to;
-        }
     }
 }
